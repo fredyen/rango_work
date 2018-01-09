9f962d488a2942a2b779f77b4a4d87427fbbed85
Fred Yen <fredyen1@gmail.com>
2018-01-09 15:43:38 -0700

    Update and test gitignore

--- tanogo_with_django_project/rango/views.py
+++ tanogo_with_django_project/rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	return render(request, 'rango/index.html', context=context_dict)
+def about(request):
+	context_dict = {'boldmessage': "AZ, MTB, Cats, Pizza, frrrrt!"}
+	return render(request, 'rango/about.html', context=context_dict)
+	#return HttpResponse("Rango says yep here is the about page. <a href='/rango/'>Index</a>")
